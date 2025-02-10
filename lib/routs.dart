36b9c4fffba47350ca8c710f57acf9289f49e910
rezaqi adib <rezaqiadib@gmail.com>
2025-02-10 11:12:56 +0200

    edit routs

--- lib/routs.dart
+++ lib/routs.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:movies/core/class/app_rout.dart';
+import 'package:movies/screen/OnBoarding.dart';
 import 'package:movies/screen/home.dart';
 import 'package:movies/screen/log.dart';
 
 Map<String, Widget Function(BuildContext)> routs = {
-//  "/": (context) => const OnBoarding(),
+  "/": (context) => const OnBoarding(),
   AppRouts.log: (context) => const Log(),
-  //AppRouts.home:
-  "/": (context) => const Home(),
+  AppRouts.home: (context) => const Home(),
 };
